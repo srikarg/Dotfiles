185dbeeb202b1369aa5d6bd8d579e8693ea2aaf4
Srikar Gudipati <srikarvgudipati@gmail.com>
2017-07-27 00:07:05 -0400

    Remove bug of adding aliases to repo folder

--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ shopt -s extglob
 # cd $dir
 
 # Backup existing dotfiles in home and then create symbolic link from home directory to this directory
-for file in !(setup.sh); do
+for file in !(setup.sh|README.md); do
     # if [[ -d "$HOME/.$file" ]] || [ -f "$HOME/.$file" ]; then
     #     echo "Backing up .$file to $olddir."
     #     mv "$HOME/.$file" $olddir
     # fi
-    echo "\nCreating symbolic link to $file in home directory.\n"
-    ln -Fis "$dir/$file" "$HOME/.$file"
+    echo "Creating symbolic link to $file in home directory.\n"
+    ln -Ffhns "$dir/$file" "$HOME/.$file"
 done
 
 source "$HOME/.bash_profile"
