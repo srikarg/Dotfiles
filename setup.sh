2c2de084742b91c518ce4ef7890fc76b052d1e68
srikarg <srikarvgudipati@gmail.com>
2023-08-07 21:46:26 -0400

    Migrate dotfiles to use GNU Stow and symlink approach instead of rsyncing into home directory

--- setup.sh
+++ setup.sh
@@ -1,44 +1,48 @@
 #!/usr/bin/env bash
 
 DOTFILES_DIR="$HOME/.Dotfiles"
-DOTFILES_BACKUP_DIR="$HOME/.Dotfiles_Old"
 ZSH_CACHE_DIR="$HOME/.cache/zsh"
 SYSTEM_TYPE=$(uname -s)
 
-git submodule init && git submodule update
+git submodule init && git submodule update --recursive --remote
 
-function doIt() {
-  printf "Installing Dotfiles to $DOTFILES_DIR and backing up old Dotfiles to $DOTFILES_BACKUP_DIR...\n"
-  rsync --exclude ".git/" \
-    --exclude ".DS_Store" \
-    --exclude ".iterm2" \
-    --exclude "setup.sh" \
-    --exclude "setupDevContainer.sh" \
-    --exclude "brew.sh" \
-    --exclude "README.md" \
-    --exclude "Dockerfile" \
-    -avP --no-perms --backup-dir="$DOTFILES_BACKUP_DIR" "$DOTFILES_DIR"/ "$HOME"
+# Check if GNU Stow is installed
+if ! which stow >/dev/null 2>&1; then
+  echo "GNU Stow is not installed. Please install it before running this script."
+  exit 1
+fi
+
+# Delete all .DS_Store files before proceeding with GNU Stow
+find "$DOTFILES_DIR" -name ".DS_Store" -type f -print -delete
+
+function run() {
+  for files in $DOTFILES_DIR/*; do
+    if [ -d "${files}" ]; then
+      stow -R "$(basename "$files")"
+      echo "$(basename "$files") stowed."
+    fi
+  done
   mkdir -p "$ZSH_CACHE_DIR"
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+  run
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo ""
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
+    run
   fi
 fi
-unset doIt
+unset run
 
 # Only set up iTerm2 on MacOS
 if [ "$SYSTEM_TYPE" = "Darwin" ]; then
   printf "Installing brew packages...\n"
-  . "$DOTFILES_DIR"/brew.sh
+  . "$DOTFILES_DIR"/homebrew/brew.sh
 
   printf "Setting up iTerm2 preferences folder...\n"
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_DIR/.iterm2"
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.iterm2"
   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 fi
 
