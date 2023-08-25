119217e8f6c38613612d088a48ddd2d2140d64dc
srikarg <srikarvgudipati@gmail.com>
2023-08-24 22:13:56 -0400

    Add filepath in status line of NeoVim

--- custom-nvchad-config/chadrc.lua
+++ custom-nvchad-config/chadrc.lua
@@ -4,6 +4,34 @@ local M = {}
 -- Path to overriding theme and highlights files
 local highlights = require('custom.highlights')
 
+-- Partially sourced from https://github.com/NvChad/ui/blob/v2.0/lua/nvchad/statusline/default.lua#L71-L87
+local getFileInfo = function()
+  local fn = vim.fn
+
+  local icon = ' 󰈚 '
+
+  local currentFilePath = fn.expand('%')
+  local currentFilePathTail = fn.expand('%:t')
+  local currentFilePathIsEmpty = currentFilePath == '' or currentFilePath == '.'
+
+  local filename = (currentFilePathIsEmpty and 'Empty ') or currentFilePathTail
+
+  -- Run ':help filename-modifiers' for an understanding of what the ':~:.' is doing
+  local filepath = (filename == 'Empty ' and filename)
+    or (' ' .. fn.fnamemodify(fn.bufname(fn.bufnr()), ':~:.'))
+
+  if filename ~= 'Empty ' then
+    local devicons_present, devicons = pcall(require, 'nvim-web-devicons')
+
+    if devicons_present then
+      local ft_icon = devicons.get_icon(filename)
+      icon = (ft_icon ~= nil and ' ' .. ft_icon) or ''
+    end
+  end
+
+  return '%#St_file_info#' .. icon .. filepath .. '%#St_file_sep#' .. ' '
+end
+
 M.ui = {
   theme = 'catppuccin',
   theme_toggle = { 'catppuccin', 'onenord_light' },
@@ -12,6 +40,14 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    theme = 'default',
+    -- The modules argument here is the default table of modules here: https://github.com/NvChad/ui/blob/v2.0/lua/nvchad/statusline/default.lua
+    overriden_modules = function(modules)
+      modules[2] = getFileInfo()
+    end,
+  },
 
   nvdash = {
     load_on_startup = false,
