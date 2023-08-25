15a7e6ccfe9dc3c557776ff223ca5a296019bfa4
srikarg <srikarvgudipati@gmail.com>
2023-08-24 22:35:50 -0400

    Update delta git pager configuration and show spaces and eol characters in NeoVim

--- custom-nvchad-config/init.lua
+++ custom-nvchad-config/init.lua
@@ -1,7 +1,12 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = '80'
+
+-- Display space and end of line characters
+vim.opt.list = true
+vim.opt.listchars:append('space:⋅')
+vim.opt.listchars:append('eol:↵')
 
 -- Auto resize panes when resizing nvim window
 autocmd('VimResized', {
