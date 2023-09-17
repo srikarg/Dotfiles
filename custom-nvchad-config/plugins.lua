adc4907087d7670924ab18c9798ec05027879c7f
srikarg <srikarvgudipati@gmail.com>
2023-09-17 11:15:27 -0500

    Load trouble plugin base64 nvchad cache

--- custom-nvchad-config/plugins.lua
+++ custom-nvchad-config/plugins.lua
@@ -216,7 +216,10 @@ local plugins = {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     cmd = { 'Trouble', 'TroubleToggle' },
-    config = true,
+    config = function()
+      dofile(vim.g.base46_cache .. 'trouble')
+      require('trouble').setup()
+    end,
   },
 
   {
