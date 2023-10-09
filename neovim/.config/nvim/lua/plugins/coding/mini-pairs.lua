0afab96f58882824c9738991fbc3412a378d9807
srikarg <srikarvgudipati@gmail.com>
2023-10-09 10:30:50 -0500

    Autopair <> with mini.pairs

--- neovim/.config/nvim/lua/plugins/coding/mini-pairs.lua
+++ neovim/.config/nvim/lua/plugins/coding/mini-pairs.lua
@@ -3,5 +3,46 @@ return {
 
   event = 'VeryLazy',
 
-  opts = {},
+  opts = {
+    modes = {
+      insert = true,
+      command = true,
+      terminal = false,
+    },
+    mappings = {
+      ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+      ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+      ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+      ['<'] = {
+        action = 'open',
+        pair = '<>',
+        neigh_pattern = '\r.',
+        register = { cr = false },
+      },
+
+      [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+      [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+      ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+      ['>'] = { action = 'close', pair = '<>', register = { cr = false } },
+
+      ['"'] = {
+        action = 'closeopen',
+        pair = '""',
+        neigh_pattern = '[^\\].',
+        register = { cr = false },
+      },
+      ["'"] = {
+        action = 'closeopen',
+        pair = "''",
+        neigh_pattern = '[^%a\\].',
+        register = { cr = false },
+      },
+      ['`'] = {
+        action = 'closeopen',
+        pair = '``',
+        neigh_pattern = '[^\\].',
+        register = { cr = false },
+      },
+    },
+  },
 }
