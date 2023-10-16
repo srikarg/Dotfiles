c0578c77486d50b3e3623bacaa5ebef028a3945a
srikarg <srikarvgudipati@gmail.com>
2023-10-16 10:22:13 -0400

    Switch from gitlinker to GBrowse functionality in vim-fugitive

--- neovim/.config/nvim/lua/plugins/editor/vim-fugitive.lua
+++ neovim/.config/nvim/lua/plugins/editor/vim-fugitive.lua
@@ -1,1 +1,32 @@
-return { 'tpope/vim-fugitive' }
+return {
+  'tpope/vim-fugitive',
+
+  lazy = false,
+
+  keys = {
+    {
+      '<leader>go',
+      ':.GBrowse <CR>',
+      '[G]it [O]pen URL for Current Line',
+    },
+
+    {
+      '<leader>go',
+      ':GBrowse <CR>',
+      '[G]it [O]pen URL for Current Line(s)',
+      mode = 'v',
+    },
+  },
+
+  dependencies = {
+    'tpope/vim-rhubarb',
+
+    {
+      'tommcdo/vim-fubitive',
+
+      config = function()
+        vim.g.fubitive_domain_pattern = 'sourcecode\\.jnj\\.com'
+      end,
+    },
+  },
+}
