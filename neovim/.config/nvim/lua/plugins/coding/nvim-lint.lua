07dad15b6cebf06287dded815c6d211c52913122
srikarg <srikarvgudipati@gmail.com>
2023-10-16 10:22:39 -0400

    Remove redundant nvim-lint eslint configuration since ESLint LSP is already enabled

--- neovim/.config/nvim/lua/plugins/coding/nvim-lint.lua
+++ neovim/.config/nvim/lua/plugins/coding/nvim-lint.lua
@@ -15,10 +15,6 @@ return {
 
   config = function()
     require('lint').linters_by_ft = {
-      ['javascript'] = { 'eslint_d' },
-      ['javascriptreact'] = { 'eslint_d' },
-      ['typescript'] = { 'eslint_d' },
-      ['typescriptreact'] = { 'eslint_d' },
       ['markdown'] = { 'markdownlint' },
       ['bash'] = { 'shellcheck' },
       ['dockerfile'] = { 'hadolint' },
