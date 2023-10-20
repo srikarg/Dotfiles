409584d12570d943f4345288888d462b9e703aea
srikarg <srikarvgudipati@gmail.com>
2023-10-20 15:27:41 -0400

    Change git signs characters to make them more visible

--- neovim/.config/nvim/lua/plugins/editor/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/editor/gitsigns.lua
@@ -5,12 +5,12 @@ return {
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = '│' },
-      change = { text = '│' },
-      delete = { text = '󰍵' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-      untracked = { text = '│' },
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+      untracked = { text = '▎' },
     },
     on_attach = function(bufnr)
       vim.keymap.set(
