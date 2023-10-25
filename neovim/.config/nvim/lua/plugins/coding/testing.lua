5bc3b4f3b7054ee668edfda6672e52cc3835b4a4
srikarg <srikarvgudipati@gmail.com>
2023-10-25 12:30:57 -0400

    Change neotest mappings

--- neovim/.config/nvim/lua/plugins/coding/testing.lua
+++ neovim/.config/nvim/lua/plugins/coding/testing.lua
@@ -3,24 +3,62 @@ return {
 
   keys = {
     {
+      '[t',
+      function()
+        require('neotest').jump.prev({ status = 'failed' })
+      end,
+      desc = 'Testing: Jump to Previous Failing Test',
+    },
+    {
+      ']t',
+      function()
+        require('neotest').jump.next({ status = 'failed' })
+      end,
+      desc = 'Testing: Jump to Next Failing Test',
+    },
+    {
       '<leader>tf',
-      ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+      function()
+        require('neotest').run.run(vim.fn.expand('%'))
+      end,
       desc = 'Testing: Run Current File',
     },
     {
       '<leader>tn',
-      ':lua require("neotest").run.run()<CR>',
+      function()
+        require('neotest').run.run()
+      end,
       desc = 'Testing: Run Nearest Test',
     },
     {
       '<leader>tt',
-      ':lua require("neotest").run.run_last()<CR>',
+      function()
+        require('neotest').run.run_last()
+      end,
       desc = 'Testing: Run Last Test',
     },
     {
       '<leader>td',
-      ':lua require("neotest").run.run({strategy = "dap"})<CR>',
+      function()
+        require('neotest').run.run({ strategy = 'dap' })
+      end,
       desc = 'Testing: Debug Nearest Test',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Testing: Toggle Summary View',
+    },
+    {
+      '<leader>to',
+      function()
+        require('neotest').output.open({
+          enter = true,
+        })
+      end,
+      desc = 'Testing: Open Output',
     },
   },
 
@@ -34,10 +72,19 @@ return {
 
   config = function()
     require('neotest').setup({
+      summary = {
+        mappings = {
+          expand = { 'e', '<2-LeftMouse>' },
+          expand_all = 'E',
+          jumpto = '<CR>',
+        },
+      },
       adapters = {
         require('neotest-jest')({
           jestCommand = 'npm test --',
-          env = { CI = true },
+          env = {
+            CI = true,
+          },
           cwd = function(path)
             return vim.fn.getcwd()
           end,
