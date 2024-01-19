07a473af84ead50dfa65759a505c790d75ba8cf0
srikarg <srikarvgudipati@gmail.com>
2024-01-19 13:56:45 -0600

    Remove conflicting symbol-usage toggle keybind as spell toggle uses the same one

--- neovim/.config/nvim/lua/plugins/coding/symbol-usage-nvim.lua
+++ neovim/.config/nvim/lua/plugins/coding/symbol-usage-nvim.lua
@@ -3,15 +3,5 @@ return {
 
   event = 'LspAttach',
 
-  keys = {
-    {
-      '<leader>us',
-      function()
-        require('symbol-usage').toggle_globally()
-      end,
-      desc = 'Toggle [S]ymbol Usage Information',
-    },
-  },
-
   config = true,
 }
