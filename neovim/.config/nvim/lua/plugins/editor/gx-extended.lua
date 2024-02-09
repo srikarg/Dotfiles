527f79ebf732412a7683c9223ab2b53f7ad1f579
srikarg <srikarvgudipati@gmail.com>
2024-02-09 11:29:57 -0600

    Create new extended-gx neovim plugin registrations
    - Go to DockerHub image page from FROM line in Dockerfile
    - Go to npmjs package from import from line in JS/TS files

--- neovim/.config/nvim/lua/plugins/editor/gx-extended.lua
+++ neovim/.config/nvim/lua/plugins/editor/gx-extended.lua
@@ -7,16 +7,42 @@ return {
     require('gx-extended').setup({
       open_fn = require('lazy.util').open,
 
+      -- Regex Character Classes in Lua Overview: https://www.lua.org/manual/5.1/manual.html#5.4.1
       extensions = {
         {
-          patterns = { '*' },
-          name = 'JIRA tickets',
+          -- This extension was initially sourced from https://github.com/rmagatti/dotfiles/blob/cac197b877883da74b6d15457fe064cd0a9d16ad/nvim/lua/rmagatti/gx-extended.lua#L8-L17
+          patterns = { '*.js', '*.jsx', '*.ts', '*.tsx' },
+          name = 'Go to npm package registry',
           match_to_url = function(line_string)
-            -- Regex Character Classes in Lua Overview: https://www.lua.org/manual/5.1/manual.html#5.4.1
+            local package_name =
+              string.match(line_string, 'from [\'"](.-)[\'"]')
+            local url = 'https://www.npmjs.com/package/' .. package_name
+
+            return package_name and url or nil
+          end,
+        },
+
+        {
+          patterns = { '*' },
+          name = 'Go to JIRA',
+          match_to_url = function(line_string)
             -- Match Jira tickets of form {4 uppercase/lowercase characters}-{1, 2, 3, or 4 digits} (ex. ABCD-1, ABCD-12, ABCD-123, ABCD-1234)
             local ticket = string.match(line_string, '%a%a%a%a%-%d%d?%d?%d?')
             local url = 'https://jira.jnj.com/browse/' .. ticket
             return ticket and url or nil
+          end,
+        },
+
+        {
+          patterns = { '*Dockerfile' },
+          name = 'Go to DockerHub',
+          match_to_url = function(line_string)
+            -- Match the image's name in FROM lines of Dockerfiles to go to the
+            -- DockerHub page for the specified image
+            local docker_image = string.match(line_string, '^FROM .-([%a%d]+):')
+            print(docker_image)
+            local url = 'https://hub.docker.com/_/' .. docker_image
+            return docker_image and url or nil
           end,
         },
       },
