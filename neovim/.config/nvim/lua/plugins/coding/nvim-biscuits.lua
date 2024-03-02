cc948bed8728582dfda048a1d3941d535d787074
srikarg <srikarvgudipati@gmail.com>
2024-03-02 12:53:36 -0600

    Add nvim-biscuits neovim plugin to show start line of closing braces via nvim-treesitter

---
+++ neovim/.config/nvim/lua/plugins/coding/nvim-biscuits.lua
@@ -0,0 +1,13 @@
+return {
+  'code-biscuits/nvim-biscuits',
+
+  event = 'VeryLazy',
+
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+
+  opts = {
+    cursor_line_only = true,
+  },
+}
