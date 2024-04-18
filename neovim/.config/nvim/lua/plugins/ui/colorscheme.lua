c85fe857a6e8635ff708739a380e2f3899c30535
srikarg <srikarvgudipati@gmail.com>
2024-04-17 21:04:32 -0400

    Make functions easier to distinguish by bolding and italicizing them

--- neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -18,7 +18,7 @@ return {
         -- Value is any valid attr-list value for `:help nvim_set_hl`
         comments = { italic = true },
         keywords = { italic = true },
-        functions = {},
+        functions = { italic = true, bold = true },
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
         sidebars = 'dark', -- style for sidebars, see below
