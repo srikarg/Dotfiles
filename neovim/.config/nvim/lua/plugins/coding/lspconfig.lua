b1bd1edf992627e0c684cd15d6bbf484d34ddb20
srikarg <srikarvgudipati@gmail.com>
2024-05-30 14:46:39 -0400

    Allow ESLint LSP to work with flat configuration files

--- neovim/.config/nvim/lua/plugins/coding/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/coding/lspconfig.lua
@@ -267,6 +267,15 @@ return {
           },
 
           eslint = {
+            settings = {
+              workingDirectories = {
+                mode = 'auto',
+              },
+              experimental = {
+                useFlatConfig = true,
+              },
+            },
+
             on_attach = function(_, bufnr)
               vim.api.nvim_create_autocmd('BufWritePre', {
                 buffer = bufnr,
