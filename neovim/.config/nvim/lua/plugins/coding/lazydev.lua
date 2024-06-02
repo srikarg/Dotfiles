985578053e2ba40a7d101632b35fa87a8129f97f
srikarg <srikarvgudipati@gmail.com>
2024-06-02 11:23:51 -0400

    Update Lazydev plugin configuration

--- neovim/.config/nvim/lua/plugins/coding/lazydev.lua
+++ neovim/.config/nvim/lua/plugins/coding/lazydev.lua
@@ -6,14 +6,26 @@ return {
 
     opts = {
       library = {
-        vim.env.LAZY .. '/luvit-meta/library',
+        'luvit-meta/library',
       },
     },
   },
 
   {
     'Bilal2453/luvit-meta',
-
     lazy = true,
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = 'lazydev',
+        -- set group index to 0 to skip loading LuaLS completions
+        group_index = 0,
+      })
+    end,
   },
 }
