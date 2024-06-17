de11531a85ee0920cc2dc6ead356469ae74ae824
srikarg <srikarvgudipati@gmail.com>
2024-06-17 15:24:00 -0400

    Add navbuddy

---
+++ neovim/.config/nvim/lua/plugins/coding/navbuddy.lua
@@ -0,0 +1,25 @@
+return {
+  'SmiteshP/nvim-navbuddy',
+
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'SmiteshP/nvim-navic',
+    'MunifTanjim/nui.nvim',
+  },
+
+  opts = {
+    lsp = {
+      auto_attach = true,
+    },
+  },
+
+  keys = {
+    {
+      '<leader>cs',
+      function()
+        require('nvim-navbuddy').open()
+      end,
+      desc = 'Symbols (Navbuddy)',
+    },
+  },
+}
