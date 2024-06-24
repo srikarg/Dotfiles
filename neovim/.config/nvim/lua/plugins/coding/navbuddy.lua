4f14e7e001b2e16320fdc81372d3f10b3ac9ec15
srikarg <srikarvgudipati@gmail.com>
2024-06-24 11:18:57 -0500

    Disable Trouble symbols keymap in favor of Navbuddy

--- neovim/.config/nvim/lua/plugins/coding/navbuddy.lua
+++ neovim/.config/nvim/lua/plugins/coding/navbuddy.lua
@@ -1,25 +1,38 @@
 return {
-  'SmiteshP/nvim-navbuddy',
+  {
+    'folke/trouble.nvim',
 
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'SmiteshP/nvim-navic',
-    'MunifTanjim/nui.nvim',
-  },
-
-  opts = {
-    lsp = {
-      auto_attach = true,
+    keys = {
+      {
+        '<leader>cs',
+        false,
+      },
     },
   },
 
-  keys = {
-    {
-      '<leader>cs',
-      function()
-        require('nvim-navbuddy').open()
-      end,
-      desc = 'Symbols (Navbuddy)',
+  {
+    'SmiteshP/nvim-navbuddy',
+
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'SmiteshP/nvim-navic',
+      'MunifTanjim/nui.nvim',
+    },
+
+    opts = {
+      lsp = {
+        auto_attach = true,
+      },
+    },
+
+    keys = {
+      {
+        '<leader>cs',
+        function()
+          require('nvim-navbuddy').open()
+        end,
+        desc = 'Symbols (Navbuddy)',
+      },
     },
   },
 }
