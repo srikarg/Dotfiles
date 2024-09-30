291224f05eb377aacbe7f93ed258056f41b49829
srikarg <srikarvgudipati@gmail.com>
2024-09-30 09:34:24 -0500

    Update neovim plugins and highlight-undo's configuration due to breaking change

--- neovim/.config/nvim/lua/plugins/ui/highlight-undo.lua
+++ neovim/.config/nvim/lua/plugins/ui/highlight-undo.lua
@@ -5,19 +5,19 @@ return {
 
   opts = {
     duration = 300,
-    undo = {
-      hlgroup = 'HighlightUndo',
-      mode = 'n',
-      lhs = 'u',
-      map = 'silent undo',
-      opts = {},
-    },
-    redo = {
-      hlgroup = 'HighlightUndo',
-      mode = 'n',
-      lhs = '<C-r>',
-      map = 'silent redo',
-      opts = {},
+    keymaps = {
+      undo = {
+        desc = 'Undo',
+        hlgroup = 'HighlightUndo',
+        mode = 'n',
+        lhs = 'u',
+      },
+      redo = {
+        desc = 'Redo',
+        hlgroup = 'HighlightUndo',
+        mode = 'n',
+        lhs = '<C-r>',
+      },
     },
     highlight_for_count = true,
   },
