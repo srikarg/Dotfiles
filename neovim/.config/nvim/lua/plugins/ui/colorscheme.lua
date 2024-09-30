54cbbb4d427f3072f16d84fa3a0a4ae9bff3f676
srikarg <srikarvgudipati@gmail.com>
2024-09-30 13:33:59 -0500

    Move colorscheme related configuration to its own file

---
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+
+    opts = {
+      on_highlights = function(highlights, colors)
+        highlights.DiagnosticUnnecessary = { fg = colors.comment }
+
+        highlights.CursorLineNr = { fg = colors.orange, bold = true }
+        highlights.LineNrAbove = { fg = colors.fg_gutter, bold = true }
+        highlights.LineNrBelow = { fg = colors.fg_gutter, bold = true }
+
+        highlights.WinSeparator = { fg = colors.comment }
+      end,
+    },
+  },
+}
