4784e41684c997a70224ebc23a2b374d2ecd8a14
srikarg <srikarvgudipati@gmail.com>
2024-10-29 14:28:46 -0500

    Add nvim-spider plugin, which provides motions for moving between words with snake_case and camelCase.

---
+++ neovim/.config/nvim/lua/plugins/editor/nvim-spider.lua
@@ -0,0 +1,26 @@
+return {
+  'chrisgrieser/nvim-spider',
+
+  opts = {},
+
+  keys = {
+    {
+      'w',
+      "<cmd>lua require('spider').motion('w')<CR>",
+      mode = { 'n', 'o', 'x' },
+      desc = 'Move to start of next of word',
+    },
+    {
+      'e',
+      "<cmd>lua require('spider').motion('e')<CR>",
+      mode = { 'n', 'o', 'x' },
+      desc = 'Move to end of word',
+    },
+    {
+      'b',
+      "<cmd>lua require('spider').motion('b')<CR>",
+      mode = { 'n', 'o', 'x' },
+      desc = 'Move to start of previous word',
+    },
+  },
+}
