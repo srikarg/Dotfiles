115f2b48d40c6ebd740b10b475e38512234a8671
srikarg <srikarvgudipati@gmail.com>
2025-01-31 11:46:10 -0600

    Update LSP config to show additional LSP symbols

--- neovim/.config/nvim/lua/plugins/coding/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/coding/nvim-lspconfig.lua
@@ -1,9 +1,40 @@
 return {
-  'neovim/nvim-lspconfig',
+  {
+    'neovim/nvim-lspconfig',
 
-  opts = {
-    inlay_hints = {
-      enabled = false,
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+
+  {
+    'LazyVim/LazyVim',
+
+    -- This controls which kinds of LSP symbols are shown querying them via a
+    -- picker like the Snacks picker. We are updating the default to include
+    -- constants and variables as well.
+    opts = {
+      kind_filter = {
+        default = {
+          'Class',
+          'Constant',
+          'Constructor',
+          'Enum',
+          'Field',
+          'Function',
+          'Interface',
+          'Method',
+          'Module',
+          'Namespace',
+          'Package',
+          'Property',
+          'Struct',
+          'Trait',
+          'Variable',
+        },
+      },
     },
   },
 }
