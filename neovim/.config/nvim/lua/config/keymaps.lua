43f162ee42b970ec0ad3d7102a94f374c81ba0da
srikarg <srikarvgudipati@gmail.com>
2025-02-01 22:51:12 -0600

    Add virtual_lines and virtual_text toggle keymap

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,11 @@
 -- Default keymaps that are always set:
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- When diagnostic lines are on, turn virtual text off and vice-versa.
+vim.keymap.set('', '<leader>bl', function()
+  vim.diagnostic.config({
+    virtual_lines = not vim.diagnostic.config().virtual_lines,
+    virtual_text = not vim.diagnostic.config().virtual_text,
+  })
+end, { desc = 'Toggle Diagnostic [l]ines' })
