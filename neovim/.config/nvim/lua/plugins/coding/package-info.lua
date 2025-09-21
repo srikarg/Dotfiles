0091f45faefb019bee2ddf7ad6d7326f0302cc36
srikarg <srikarvgudipati@gmail.com>
2025-09-21 15:50:14 -0500

    Update package-info nvim plugin

--- neovim/.config/nvim/lua/plugins/coding/package-info.lua
+++ neovim/.config/nvim/lua/plugins/coding/package-info.lua
@@ -19,9 +19,15 @@ return {
     },
 
     opts = {
-      colors = {
-        up_to_date = '#3C4048', -- Text color for up to date package virtual text
-        outdated = '#fc514e', -- Text color for outdated package virtual text
+      highlights = {
+        -- Text color for up to date package virtual text
+        up_to_date = {
+          fg = '#3C4048',
+        },
+        -- Text color for outdated package virtual text
+        outdated = {
+          fg = '#fc514e',
+        },
       },
       icons = {
         enable = true, -- Whether to display icons
