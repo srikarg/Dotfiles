6e68749a0ceee9e22966f27a7eee8739c4dc3ab5
srikarg <srikarvgudipati@gmail.com>
2025-09-25 18:34:41 -0500

    Update neovim plugins and switch to native LSP Neovim completion for copilot/other LSPs

--- neovim/.config/nvim/lua/plugins/coding/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/coding/nvim-lspconfig.lua
@@ -2,11 +2,28 @@ return {
   {
     'neovim/nvim-lspconfig',
 
-    opts = {
-      inlay_hints = {
+    opts = function(_, opts)
+      opts.inlay_hints = {
         enabled = false,
-      },
-    },
+      }
+
+      -- https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
+      local keys = require('lazyvim.plugins.lsp.keymaps').get()
+
+      keys[#keys + 1] = {
+        '<S-CR>',
+        function()
+          if not vim.lsp.inline_completion.get() then
+            return '<S-CR>'
+          end
+        end,
+        mode = { 'i' },
+        expr = true,
+        desc = 'Accept the current inline completion',
+      }
+
+      return opts
+    end,
   },
 
   {
